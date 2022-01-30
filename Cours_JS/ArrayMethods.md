bae88f4daeb181b46e35650fd7f1e611479a7a35
Tony Marques <tony.marques@live.fr>
2022-01-31 00:16:14 +0100

    ajout fichier

---
+++ Cours_JS/ArrayMethods.md
@@ -0,0 +1,7 @@
+# Les méthodes sur les tableaux
+```js
+let arr = [1, 2, 3];
+arr.forEach((item, index) => {
+  console.log(item, index); // item: 1 2 3 // index 0 1 2
+});
+```
---
+++ Cours_JS/arrayMethods.js
@@ -0,0 +1,5 @@
+// forEach(): itérer pour chaque item du tableau
+let arr = [1, 2, 3];
+arr.forEach((item, index) => {
+  console.log(item, index); // item: 1 2 3 // index 0 1 2
+});
